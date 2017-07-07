cee662b2569dc547c217d23f7de5f73dc590f2c8
Aksharakeerthi <akshara2223@gmail.com>
2017-07-07 15:59:29 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,17 @@
+# sum
+import java.io.*;
+import java.util.*;
+public class sum{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int k,i=1;
+int sum=0;
+System.out.println("Enter the sum");
+k=sc.nextInt();
+while(i<=k){
+sum=sum+k;
+kk;
+}
+System.out.println("The sum of the number is"+sum);
+}
+}
